2fe6b9dfdeb23e16748ce4fac219ba3d2e2721fb
usman <usmanabdat6@gmail.com>
2022-11-02 14:44:14 +0700

    start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'assets/css/app.css'
+
+function App() {
+  return (
+    <div className="bg-red-600">
+      <h1 className="text-3xl text-red-100">Text</h1>
+    </div>
+    
+  );
+}
+
+export default App;
