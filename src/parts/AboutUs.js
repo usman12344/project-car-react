0b7d2989c69337902f5b7547cec5d77cd7d6b329
usman <usmanabdat6@gmail.com>
2022-11-26 10:20:05 +0700

    fix commit

--- src/parts/AboutUs.js
+++ src/parts/AboutUs.js
@@ -9,9 +9,9 @@ export default function AboutUs() {
         </div>
         <div className="absolute md:relative md:pt-28 md:-ml-10 md:visible invisible md:w-auto w-0">
           <h2 className="text-white font-bold  text-4xl text-center justify-start items-center flex">RENTAL CAR</h2>
-          <a href="#">
+          <button href="">
             <h2 className="text-white font-semibold text-yellow-100 text-2xl text-center justify-end items-center flex mt-10 hover:text-white">Follow Us</h2>
-          </a>
+          </button>
         </div>
       </div>
     </section>
--- src/parts/Header.js
+++ src/parts/Header.js
@@ -19,6 +19,7 @@ export default function Header({ position }) {
           setCartChanged(false)
       }, 550);
     }
+     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [state.cart])
 
 
